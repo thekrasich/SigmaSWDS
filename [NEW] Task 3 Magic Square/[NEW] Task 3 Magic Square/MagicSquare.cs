4cf54843cc18c9d43fe6e5b4c6d2060a35d44de7
thekrasich <91842995+thekrasich@users.noreply.github.com>
2021-10-06 19:28:38 +0300

    Add files via upload

---
+++ [NEW] Task 3 Magic Square/[NEW] Task 3 Magic Square/MagicSquare.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _NEW__Task_3_Magic_Square
+{
+    class MagicSquare: Matrix
+    {
+        
+
+        public void MagicSquareGenerate(int size)
+        {
+            _size = size;
+            if (_size>0 && _size % 2 != 0)
+            {
+
+                int x = _size / 2;
+                int y = _size - 1;
+                int i = 1;
+
+
+                while(i <= _size * _size)
+                {
+
+                    if(x == -1 && y == _size)
+                    {
+                        x = 0;
+                        y = _size - 2;
+                    }
+                    else
+                    {
+                        if (y == _size)
+                        {
+                            y = 0;
+                        }
+                        if (x < 0)
+                        {
+                            x = _size - 1;
+                        }
+
+                    }
+                    if(_matrix[x,y]!=0)
+                    {
+                        y -= 2;
+                        x++;
+                    }
+                    else
+                    {
+                        _matrix[x, y] = i++;
+
+
+                        x--;
+                        y++;
+                    }
+
+
+
+
+                }
+
+
+
+
+
+
+
+
+
+            }
+            else
+            {
+                _size = 1;
+                for(int i = 0; i < _size; i++)
+                {
+                    for(int j = 0; j <_size; j++)
+                    {
+                        _matrix[i, j] = 0;
+                    }
+                   
+                }
+            }
+
+
+        }
+
+
+        //Test
+        /*
+        public void Print()
+        {
+            for (int i = 0; i < _size; i++)
+            {
+                for (int j = 0; j < _size; j++)
+                {
+                    Console.Write($"{_matrix[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+        */
+
+    }
+}
---
+++ [NEW] Task 3 Magic Square/[NEW] Task 3 Magic Square/MatrixPrint.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _NEW__Task_3_Magic_Square
+{
+    class MatrixPrint
+    {
+
+
+
+
+        public void PrintMatrix(Matrix obj)
+        {
+
+            for(int i = 0; i< obj.Size; i++)
+            {
+                for (int j = 0; j < obj.Size; j++)
+                {
+
+                    Console.Write($"{obj.GetElement(i,j)} ");
+
+
+
+                }
+                Console.WriteLine();
+
+            }
+
+
+
+        }
+
+
+    }
+}
