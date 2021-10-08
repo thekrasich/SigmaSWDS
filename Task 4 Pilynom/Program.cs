f6c7e94d0a3d9ab9512300f7c21b56753fdbd61b
thekrasich <91842995+thekrasich@users.noreply.github.com>
2021-10-08 16:09:39 +0300

    Add files via upload

---
+++ Task 4 Pilynom/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Task_4_Pilynom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+           Polynomial y = Polynomial.Parse("10x^3+5x^2+5");
+           Polynomial x = Polynomial.Parse("5x^3+3x^2-3");
+
+            y.PolynomPrint();
+            Console.WriteLine();
+            x.PolynomPrint();
+
+            Polynomial z = new Polynomial();
+            z = z.PolynomSubstraction(x, y);
+            Console.WriteLine();
+            z.PolynomPrint();
+        }
+    }
+}
---
+++ Task 4 Pilynom/StringUtilities.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task_4_Pilynom
+{
+    class StringUtilities
+    {
+        public static string Reverse(string s)
+        {
+            char[] charArray = s.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+    }
+}
