cd514514c3c4f67ef68a4d8789d8c951e1889ead
thekrasich <91842995+thekrasich@users.noreply.github.com>
2021-10-09 11:36:54 +0300

    Add files via upload

---
+++ Task 5.3 FilePath/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Task_5._3_Sigma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string example = @"'c: \ WebServers \ home \ testsite \ www \ myFile.Name57.txt'";
+            string answer;
+
+            Console.WriteLine($"Example String : {example} \n");
+
+
+            answer = StringPathUtilities.StringFindFileName(example);
+            Console.WriteLine($"File Name without expansion: {answer} \n");
+
+            answer = StringPathUtilities.StringFindRootFolder(example);
+            Console.WriteLine($"Root Folder of file: {answer} \n");
+
+
+        }
+    }
+}
